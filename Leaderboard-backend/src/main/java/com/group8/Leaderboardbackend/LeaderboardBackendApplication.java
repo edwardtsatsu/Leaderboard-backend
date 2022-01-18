b40c4689fe4cfcc0b271f38bc469d84c4ba7af5a
Akorlie Edward Tsatsu <65318539+edwardtsatsu@users.noreply.github.com>
2022-01-18 14:18:24 +0000

    Initial commit

---
+++ Leaderboard-backend/src/main/java/com/group8/Leaderboardbackend/LeaderboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.group8.Leaderboardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaderboardBackendApplication.class, args);
+	}
+
+}
---
+++ Leaderboard-backend/src/test/java/com/group8/Leaderboardbackend/LeaderboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group8.Leaderboardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaderboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
