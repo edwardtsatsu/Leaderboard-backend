3a4d728e4ea0d43d54e3f4bcaf70ab4811ae4f25
Akorlie Edward Tsatsu <65318539+edwardtsatsu@users.noreply.github.com>
2022-01-18 15:49:15 +0000

    repository was updated

--- Leaderboard-backend/src/main/java/com/group8/Leaderboardbackend/repository/ProfileRepository.java
+++ Leaderboard-backend/src/main/java/com/group8/Leaderboardbackend/repository/ProfileRepository.java
@@ -1,8 +1,9 @@
 package com.group8.Leaderboardbackend.repository;
+import com.group8.Leaderboardbackend.model.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, String> {
-
 
 }
