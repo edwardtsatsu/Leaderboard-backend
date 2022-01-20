c21c6ba29b9c26ab2651f6241da56401bad85479
Justice Owusu Asare <91653263+justice-asare@users.noreply.github.com>
2022-01-20 14:13:05 +0000

    Delete Leaderboard-backend/src/main/java/com/group8/Leaderboardbackend/exception directory

--- Leaderboard-backend/src/main/java/com/group8/Leaderboardbackend/exception/UserDoesNotExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.group8.Leaderboardbackend.exception;
-
-public class UserDoesNotExistsException extends RuntimeException{
-    public UserDoesNotExistsException(String msg) {
-        super((msg));
-    }
-}
